be10ea159d926b492c4e45576a8a05aca3c8cf31
anthonyPassler <passleranthony@yahoo.com>
2023-03-26 22:07:04 +0200

    feat: add additional columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_185056) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_200247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -42,6 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_185056) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "subscribed", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
