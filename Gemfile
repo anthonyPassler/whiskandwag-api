9a746369d21163362cfc350d45bb829258bff088
anthonyPassler <passleranthony@yahoo.com>
2023-03-29 21:23:16 +0200

    feat: add rubocop rspec

--- Gemfile
+++ Gemfile
@@ -44,7 +44,11 @@ gem "bootsnap", require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
 
-gem "rubocop-rails", require: false
+group :rubocop do
+  gem "rubocop", require: false
+  gem "rubocop-rails", require: false
+  gem "rubocop-rspec", require: false
+end
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- spec/requests/api/v1/recipes_controller_spec.rb
+++ spec/requests/api/v1/recipes_controller_spec.rb
@@ -3,17 +3,20 @@
 require "swagger_helper"
 
 RSpec.describe "api/v1/recipes_controller", type: :request do
-  path "/api/v1/recipes" do
-    get("list recipes") do
-      response(200, "successful") do
-        after do |example|
-          example.metadata[:response][:content] = {
-            "application/json" => {
-              example: JSON.parse(response.body, symbolize_names: true)
+  it "returns a list of recipes" do
+    path "/api/v1/recipes" do
+      get("list recipes") do
+        response(200, "successful") do
+          after do |example|
+            example.metadata[:response][:content] = {
+              "application/json" => {
+                example: JSON.parse(response.body, symbolize_names: true)
+              }
             }
-          }
+          end
+
+          run_test!
         end
-        run_test!
       end
     end
   end
